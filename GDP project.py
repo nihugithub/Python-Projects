1938813ea95fd74425e5f416530cec700f6d3550
nihugithub <41658331+nihugithub@users.noreply.github.com>
2020-06-12 21:34:01 +1000

    Update GDP project.py

--- GDP project.py
+++ GDP project.py
@@ -1,9 +1,7 @@
 
 
-# 
-
-# <h1>Analyzing US Economic Data and  Building a Dashboard  </h1>
-# <h2>Description</h2>
+#Analyzing US Economic Data and  Building a Dashboard  </h1>
+# Description
 # 
 
 # Extracting essential data from a dataset and displaying it is a necessary part of data science; therefore individuals can make correct decisions based on the data. In this assignment, you will extract some essential economic indicators from some data, you will then display these economic indicators in a Dashboard. You can then share the dashboard via an URL.
